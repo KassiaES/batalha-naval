0361945300b19c595098ca5ef46b48b68a3a97dc
Kassia do Espirito Santo <kassiaes@gmail.com>
2022-12-27 16:16:15 -0300

    Add files via upload

---
+++ src/utils/LeitorTeclado.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public abstract class LeitorTeclado {
+    static Scanner entrada = new Scanner(System.in);
+    public static int getNumero(String frase) {
+
+        System.out.print(frase);
+        try {
+            return entrada.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Digite uma opção válida.");
+            return getNumero(frase);
+        }
+    }
+
+
+}
